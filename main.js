7de71ad7f5dbcc49f7c525f30610db13ae409681
Alejandra M <alejandra.mejia.3c@gmail.com>
2021-12-14 10:35:35 +0100

    add html, css and js

---
+++ main.js
@@ -0,0 +1,80 @@
+"use strict";
+
+/* Constante y variables */
+
+const playButton = document.querySelector(".play__button");
+const option = document.querySelector(".select__move");
+let messageText = document.querySelector(".message__text");
+let countPlayer = document.querySelector(".player__count");
+let countPc = document.querySelector(".pc__count");
+let count = "";
+
+/* Función que genera un número aleatorio  para definir la jugada del pc*/
+let pcMove = "";
+function getRandomNumber(max) {
+  return Math.ceil(Math.random() * max);
+}
+function generatePcMove() {
+  pcMove = getRandomNumber(8);
+  if (pcMove >= 0 && pcMove < 3) {
+    pcMove = "Piedra";
+  } else if (pcMove >= 3 && pcMove < 6) {
+    pcMove = "Papel";
+  } else if (pcMove >= 6 && pcMove <= 8) {
+    pcMove = "Tijera";
+  }
+  console.log(`El pc ha usado ${pcMove}`);
+}
+
+/* Función que compara la jugada del pc con la de la usuaria*/
+
+function fightMoves() {
+  let userMove = option.value;
+  if (userMove === pcMove) {
+    messageText.innerHTML = `Empate`;
+  } else if (userMove === "Piedra" && pcMove === "Papel") {
+    messageText.innerHTML = `Has perdido`;
+  } else if (userMove === "Piedra" && pcMove === "Tijera") {
+    messageText.innerHTML = `Has ganado`;
+  } else if (userMove === "Papel" && pcMove === "Tijera") {
+    messageText.innerHTML = `Has perdido`;
+  } else if (userMove === "Papel" && pcMove === "Piedra") {
+    messageText.innerHTML = `Has ganado`;
+  } else if (userMove === "Tijera" && pcMove === "Piedra") {
+    messageText.innerHTML = `Has perdido`;
+  } else if (userMove === "Tijera" && pcMove === "Papel") {
+    messageText.innerHTML = `Has ganado`;
+  }
+}
+
+/* Función del contador de la jugadora*/
+function countTries() {  
+  if (messageText.innerHTML === `Has ganado`) {
+    count++;
+    countPlayer.innerHTML = `Jugador: ${count} `;
+  } else if (messageText.innerHTML === `Has perdido`) {
+    count++;
+    countPc.innerHTML = `Computadora: ${count} `;
+  }
+}
+/* Función de reseteo a los 10 intentos */
+let clickNumber = "";
+function countingClicks(event) {
+  if (event.currentTarget) {
+    clickNumber++;
+   
+  }
+}
+function reset() {
+  if (clickNumber > 10) {
+    location.reload();
+  }
+}
+
+/* Eventos */
+playButton.addEventListener("click", getRandomNumber);
+playButton.addEventListener("click", generatePcMove);
+playButton.addEventListener("click", fightMoves);
+playButton.addEventListener("click", countTries);
+playButton.addEventListener("click", countingClicks);
+playButton.addEventListener("click", reset);
